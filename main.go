86ab8166f174e30827a887e105562f2ccfc6772a
Eason chiu <76670758@qq.com>
2020-02-20 17:08:37 +0800

    fix-bug

--- main.go
+++ main.go
@@ -46,8 +46,9 @@ func main() {
   keyvars := feather.GetKeyVarsFromFile(vars)
   markdown, json := engine.Start(title, filenames, keyvars)
 
-  _= ioutil.WriteFile(outfile+".md", []byte(markdown), os.ModePerm)
   if jsonFile {
-    _= ioutil.WriteFile(outfile+".api.json", []byte(json), os.ModePerm)
+    _= ioutil.WriteFile(outfile+".go.json", []byte(json), os.ModePerm)
+  } else {
+    _= ioutil.WriteFile(outfile+".md", []byte(markdown), os.ModePerm)
   }
 }
